846f977062e1477bd23e7a168bef0e8b00816472
Deva <>
2022-08-06 14:13:42 +0100

    Update {{name}}_view_model.dart

--- bricks/custom_feature/__brick__/{{name}}/{{name}}_view_model.dart
+++ bricks/custom_feature/__brick__/{{name}}/{{name}}_view_model.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 part '{{name}}_view_state.dart';
 
-class {{name.pascalCase()}}ViewModel extends StateNotifier<MySuperViewState> {
+class {{name.pascalCase()}}ViewModel extends StateNotifier<{{name.pascalCase()}}ViewState> {
   {{name.pascalCase()}}ViewModel() : super({{name.pascalCase()}}ViewState());
 }
